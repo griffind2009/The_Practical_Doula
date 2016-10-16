ac6a02965ae3155e1b01d0bdb78441c1651165e3
Domonique Griffin <griffind2009@gmail.com>
2016-10-15 22:41:25 -0400

    most crud done for client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -13,10 +13,24 @@ class ClientsController < ApplicationController
 
   def create
     @client = Client.create(client_params)
+    redirect_to @client
   end
-end
+
+  def update
+    @client = Client.find(params[:id])
+    @client.update(client_params)
+    redirect_to @client
+  end
+
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+    redirect_to clients_path
+  end
+
 
 private
 def client_params
   params.require(:client).permit(:name, :chart_type)
+end
 end
