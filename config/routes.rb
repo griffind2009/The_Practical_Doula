bb76aed50dc48882d6f36c6f58d74982684f0559
Domonique Griffin <griffind2009@gmail.com>
2016-10-20 09:10:02 -0400

    added a welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'clients#index'
+root to: 'welcome#index'
 
 resources :clients, shallow: true do
 resources :basics, only: [:index, :new, :create]
